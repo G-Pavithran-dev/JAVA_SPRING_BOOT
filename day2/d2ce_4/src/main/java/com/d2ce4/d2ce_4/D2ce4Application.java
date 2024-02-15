15d4f72fabd3fe7a1071aad3b2682a65ccde2dda
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2024-02-15 09:28:51 +0530

    day 2 upload

---
+++ day2/d2ce_4/src/main/java/com/d2ce4/d2ce_4/D2ce4Application.java
@@ -0,0 +1,13 @@
+package com.d2ce4.d2ce_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2ce4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2ce4Application.class, args);
+	}
+
+}
---
+++ day2/d2ce_4/src/test/java/com/d2ce4/d2ce_4/D2ce4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d2ce4.d2ce_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2ce4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
