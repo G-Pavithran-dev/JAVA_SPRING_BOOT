2cf9b80104a00fca7118415980d558cbd288d51f
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2024-06-11 19:37:16 +0530

    chore: Update Java version to 17 in pom.xml files

--- day7/Person (d7ce1)/springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ day7/Person (d7ce1)/springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -2,12 +2,19 @@ package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+// @EnableSwagger2
 public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
 	}
-
 }
